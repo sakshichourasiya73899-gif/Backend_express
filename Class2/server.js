9adc9058ba493b4c4646af6cac707212df9f3641
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-01-22 00:18:16 +0530

    Updaded Server

--- Class2/server.js
+++ Class2/server.js
@@ -6,6 +6,9 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.send('About Us Page')
 })
+app.get('/contact',(req,res)=>{
+    res.send('Contact Us Page')
+})
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
